8f5d0ea5dd26553f18e018e757e182a1d5734c5b
NadinFedoriv <nadinfedoriv@gmail.com>
2023-06-20 19:46:45 +0300

    task add

---
+++ src/components/App/App.jsx
@@ -0,0 +1,23 @@
+import { Profile } from '../Profile/Profile';
+import { Statistics } from '../Statistics/Statistics';
+
+
+import user from 'user.json';
+import css from './App.module.css';
+
+console.log(css);
+
+
+export const App = () => {
+  return (
+    <div className={css.container}>
+      <Profile username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}/>
+      <Statistics />
+      
+    </div>
+  );
+};
